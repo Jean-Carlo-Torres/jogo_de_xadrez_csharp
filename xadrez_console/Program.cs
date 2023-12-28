b443e20c68a4730a9647c9776bcad1a46c05ebaa
Jean Carlo <jeansanto2002@gmail.com>
2023-12-28 10:21:47 -0300

    Começando a colocar peças no tabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using xadrez_console.tabuleiro;
+using xadrez_console.xadrez;
 
 namespace xadrez_console
 {
@@ -7,6 +8,10 @@ namespace xadrez_console
         public static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.ImprimirTabuleiro(tab);
 
--- xadrez_console/tabuleiro/Tabuleiro.cs
+++ xadrez_console/tabuleiro/Tabuleiro.cs
@@ -18,5 +18,10 @@ namespace xadrez_console.tabuleiro
         {
             return Pecas[linha, coluna];
         }
+
+        public void ColocarPeca(Peca peca, Posicao posicao){
+            Pecas[posicao.Linha, posicao.Coluna] = peca;
+            peca.Posicao = posicao;
+        }
     }
 }
